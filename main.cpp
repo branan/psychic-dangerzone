754cc445d6f27b1da5ba67ad12152eee47566fde
Nick Lewis <nick@puppetlabs.com>
2015-03-11 21:31:01 -0700

    Add a player circle that can move

--- main.cpp
+++ main.cpp
@@ -8,19 +8,40 @@ int main() {
 
     sf::Clock world_clock;
 
+    sf::CircleShape player(50);
+
     float last_frame_time;
     while(window.isOpen()) {
         last_frame_time = world_clock.getElapsedTime().asSeconds();
 
         sf::Event event;
         while(window.pollEvent(event)) {
-            // TODO: process events
             if(event.type == sf::Event::Closed) {
                 window.close();
             }
         }
 
+        sf::Vector2f velocity;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+          velocity.x -= 10;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+          velocity.x += 10;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+          velocity.y -= 10;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+          velocity.y += 10;
+        }
+
+        player.move(velocity);
+        //player.move(velocity.x, velocity.y);
+
         window.clear(sf::Color::Black);
+
+        window.draw(player);
+
         window.display();
         std::cout << world_clock.getElapsedTime().asSeconds() - last_frame_time << std::endl;
     }
