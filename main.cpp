7d0d99db351c57a2e70fc90f20236f4c87830371
Branan Riley <branan@puppetlabs.com>
2015-03-11 21:12:32 -0700

    Add frame time output

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/System/Clock.hpp>
+#include <iostream>
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(800,600),  "Psychic Dangerzone");
+    window.setVerticalSyncEnabled(true);
 
+    sf::Clock world_clock;
+
+    float last_frame_time;
     while(window.isOpen()) {
+        last_frame_time = world_clock.getElapsedTime().asSeconds();
+
         sf::Event event;
         while(window.pollEvent(event)) {
             // TODO: process events
@@ -14,6 +22,7 @@ int main() {
 
         window.clear(sf::Color::Black);
         window.display();
+        std::cout << world_clock.getElapsedTime().asSeconds() - last_frame_time << std::endl;
     }
 
     return 0;
